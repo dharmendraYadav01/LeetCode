f57ec9fe81a92d68f3e7dad115269b2d57bff2ce
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-07-30 15:09:07 +0530

    LC-minimum-number-of-pushes-to-type-word-i [Runtime: 3 ms; Memory: 8.2 MB]

--- math/minimum-number-of-pushes-to-type-word-i.cpp
+++ math/minimum-number-of-pushes-to-type-word-i.cpp
@@ -4,11 +4,15 @@ public:
         int n = word.length();
         int ans = 0;
         int k = 1;
-        while (n / 8) {
+        for (int i = 0; i < n / 8; i++) {
             ans += k * 8;
             k += 1;
-            n %= 8;
         }
+        // while (n / 8) {
+        //     ans += k * 8;
+        //     k += 1;
+        //     n /= 8;
+        // }
         if (n) {
             ans += k * (n % 8);
         }
