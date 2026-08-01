03599658041b8d4747eb1b61549b1d51d7a6e98c
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-01 08:44:56 +0530

    LC-predict-the-winner [Runtime: N/A; Memory: N/A]

--- dynamic-programming/predict-the-winner.cpp
+++ dynamic-programming/predict-the-winner.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
-        if (nums.size() % 2 == 0)
+        
+        if (nums.size() % 2 == 0 || nums.size()==1)
             return true;
         return false;
     }
