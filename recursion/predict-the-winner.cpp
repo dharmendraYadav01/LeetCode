4a1bf14f7610741e4357612a430ec935c35ff4dd
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-01 23:27:33 +0530

    LC-predict-the-winner [Runtime: N/A; Memory: N/A]

--- recursion/predict-the-winner.cpp
+++ recursion/predict-the-winner.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int ans = 0;
+    int dp[23][23];
     int solve(vector<int>& nums, int i, int j) {
         if (i == j)
             return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
         int left = nums[i] - solve(nums, i + 1, j);
         int right = nums[j] - solve(nums, i, j - 1);
-        return max(left, right);
+        return dp[i][j] = max(left, right);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        memset(dp, -1, sizeof(dp));
         return solve(nums, 0, n - 1) >= 0;
     }
 };
