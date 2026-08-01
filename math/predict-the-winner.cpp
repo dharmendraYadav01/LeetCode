b1b3bbf1d26b8cadcad66e7ae302e4cb0ce1bc79
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-01 23:12:43 +0530

    LC-predict-the-winner [Runtime: N/A; Memory: N/A]

--- math/predict-the-winner.cpp
+++ math/predict-the-winner.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    int ans = 0;
+    int solve(vector<int>& nums, int i, int j) {
+        if (i == j)
+            return 0;
+        int left = nums[i] - solve(nums, i + 1, j);
+        int right = nums[j] - solve(nums, i, j - 1);
+        return max(left, right);
+    }
     bool predictTheWinner(vector<int>& nums) {
-        
-        if (nums.size() % 2 == 0 || nums.size()==1)
-            return true;
-        return false;
+        int n = nums.size();
+        return solve(nums, 0, n - 1) >= 0;
     }
 };
