3448baab4a683b5dd48daa72d9e17bc38fa125b4
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-02 18:07:31 +0530

    LC-binary-tree-maximum-path-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/binary-tree-maximum-path-sum.cpp
+++ dynamic-programming/binary-tree-maximum-path-sum.cpp
@@ -6,23 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int max_sum=INT_MIN;
-    int dfs(TreeNode* root){
-        if(!root) return 0;
-        int l=max(dfs(root->left),0);
-        int r=max(dfs(root->right),0);
-        int c_sum=root->val+l+r;
-        max_sum=max(c_sum,max_sum);
-        return root->val+max(l,r);
-        
+    int mx_s = 0;
+    int maxSum(TreeNode* root) {
+        if (!root)
+            return 0;
+        int left = maxSum(root->left);
+        int right = maxSum(root->right);
+        int ans = 0;
+        ans = left + root->val + right;
+        mx_s = max(mx_s, ans);
+        return ans;
     }
     int maxPathSum(TreeNode* root) {
-        dfs(root);
-        return max_sum;
+        maxSum(root);
+        return mx_s;
     }
 };
