58fb90aed26adb94adc13926dfe78c73f435e714
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-03 14:47:12 +0530

    LC-stone-game-iii [Runtime: N/A; Memory: N/A]

---
+++ game-theory/stone-game-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(vector<int>& stoneValue, int i) {
+        int ans = INT_MIN;
+        int n = stoneValue.size();
+        if (i >= n)
+            return 0;
+        ans = max(ans, stoneValue[i] - solve(stoneValue, i + 1));
+        if (i + 1 < n)
+            ans = max(ans, stoneValue[i] + stoneValue[i + 1] - solve(stoneValue, i + 2));
+        if (i + 2 < n)
+            ans = max(ans, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - solve(stoneValue, i + 3));
+        return ans;
+    }
+    string stoneGameIII(vector<int>& stoneValue) {
+        int diff = solve(stoneValue, 0);
+        if (diff > 0)
+            return "Alice";
+        else if (diff < 0)
+            return "Bob";
+        return "Tie";
+    }
+};
