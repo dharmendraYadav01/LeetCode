37402277ef0772003ea5ccf17c67e711b53d5471
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-03 15:01:27 +0530

    LC-stone-game-iii [Runtime: N/A; Memory: N/A]

--- zero-sum-game/stone-game-iii.cpp
+++ zero-sum-game/stone-game-iii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int dp[1001];
+    int dp[100003];
     int solve(vector<int>& stoneValue, int i) {
         int ans = INT_MIN;
         int n = stoneValue.size();
         if (i >= n)
             return 0;
-        if(dp[i]!=-1) return dp[i];
+        if(dp[i]!=INT_MIN) return dp[i];
         ans = max(ans, stoneValue[i] - solve(stoneValue, i + 1));
         if (i + 1 < n)
             ans = max(ans, stoneValue[i] + stoneValue[i + 1] - solve(stoneValue, i + 2));
@@ -15,7 +15,7 @@ public:
         return dp[i]=ans;
     }
     string stoneGameIII(vector<int>& stoneValue) {
-        memset(dp,-1,sizeof(dp));
+        fill(dp,dp+100003,INT_MIN); // instead of memset(dp,-1,sizeof(dp)) we use fill(dp,dp+100003,INT_MIN);
         int diff = solve(stoneValue, 0);
         if (diff > 0)
             return "Alice";
