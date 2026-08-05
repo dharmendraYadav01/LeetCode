03601d1e4a9977b67c96f84ee3ebdfca86281ea2
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-05 23:01:02 +0530

    LC-remove-methods-from-project [Runtime: 675 ms; Memory: 308.3 MB]

--- graph/remove-methods-from-project.cpp
+++ graph/remove-methods-from-project.cpp
@@ -3,7 +3,7 @@ public:
     void dfs(int k, vector<int>& visit, vector<vector<int>>& adj) {
         visit[k] = 1;
         for (int node : adj[k]) {
-            if (!visit[node]){
+            if (!visit[node]) {
                 dfs(node, visit, adj);
             }
         }
@@ -16,18 +16,17 @@ public:
         vector<int> visit(n, 0);
         vector<int> ans;
         dfs(k, visit, adj);
-        for(auto& it:invocations){
-            int u=it[0];
-            int v=it[1];
-            if(!visit[u] && visit[v]){
-                visit[v]=0;
-            }
-            if(visit[u] && !visit[v]){
-                visit[u]=0;
+        bool flag = true;
+        for (auto& it : invocations) {
+            int u = it[0];
+            int v = it[1];
+            if (!visit[u] && visit[v]) {
+                flag = false; // agar essa koi pair hai toh sare non-suspicious hoge
+                break;
             }
         }
         for (int i = 0; i < visit.size(); i++) {
-            if (visit[i] == 0) {
+            if (!flag || visit[i] == 0) {
                 ans.push_back(i);
             }
         }
