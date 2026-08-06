ee06380ac4e8a29be6a6b7ca7e2b745c31cc0de1
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-06 19:36:14 +0530

    LC-smallest-divisible-digit-product-i [Runtime: N/A; Memory: N/A]

--- enumeration/smallest-divisible-digit-product-i.cpp
+++ enumeration/smallest-divisible-digit-product-i.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int digit(int n){
-        int ans=1;
-        while(n!=0){
-            ans*=n%10;
-            n/=10;
+    int digit(int n) {
+        int ans = 1;
+        while (n != 0) {
+            ans *= n % 10;
+            n /= 10;
         }
         return ans;
     }
     int smallestNumber(int n, int t) {
-        while(true){
-            int k=digit(n);
-            if(k%t==0) return n;
+        while (true) {
+            int k = digit(n);
+            if (k % t == 0)
+                return n;
             n++;
         }
         return 0;
