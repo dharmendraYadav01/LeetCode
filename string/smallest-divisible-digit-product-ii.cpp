c84f9518670426f9ebf54be9bedb401e3c120b89
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-08 00:53:54 +0530

    LC-smallest-divisible-digit-product-ii [Runtime: N/A; Memory: N/A]

--- string/smallest-divisible-digit-product-ii.cpp
+++ string/smallest-divisible-digit-product-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     typedef long long ll;
-    int gcd(int a, int b) {
+    int gcd(ll a, ll b) {
         if (b == 0)
             return a;
         return gcd(b, a % b);
     }
-    bool isGood(long long t) {
+    bool isGood(ll t) {
         for (int i = 2; i <= 9; i++) {
             while ((t % i) == 0) {
                 t /= i;
             }
         }
-        return t == 1 ? 1 : 0;
+        return t == 1;
     }
     string func(ll num, int t) {
         string ans;
@@ -28,30 +28,35 @@ public:
         return ans;
     }
     string smallestNumber(string num, long long t) {
-        //
-        int n = num.length();
         if (!isGood(t))
             return "-1";
 
-        //
-        int mul = 1;
-        for (int i = 0; i < num.size(); i++) {
-            int d = num[i] - '0';
-            if (d == 0)
+    // 1. Check if 'num' itself is already valid
+        int n = num.length();
+        bool flag = false;
+        ll det = t;
+        for (char c : num) {
+            if (c == '0') {
+                flag = true;
                 break;
-            mul *= d;
-            if (mul % t == 0)
-                return num;
+            }
+            det = det / gcd(det, c - '0');
         }
+        if (!flag && det == 1)
+            return num;
 
-        //
+    // 2. Find the first zero in 'num'
         int zeroIdx = num.length() - 1;
         for (int i = 0; i < num.length(); i++) {
-            if (num[i] == '0')
+            if (num[i] == '0') {
                 zeroIdx = i;
+                break;
+            }
         }
+
+    // 3. Precompute remaining factor requirements up to first zero
         vector<ll> remain(n + 1, t);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < zeroIdx; i++) {
             remain[i + 1] = remain[i] / gcd(remain[i], num[i] - '0');
         }
 
@@ -59,7 +64,7 @@ public:
             ll req = remain[i];
             int freeSlot = n - i - 1;
             for (int d = (num[i] - '0') + 1; d <= 9; d++) {
-                int freq = req / gcd(req, d);
+                ll freq = req / gcd(req, d);
                 string rem = func(freq, freeSlot);
                 if (rem.length() == freeSlot)
                     return num.substr(0, i) + char(d + '0') + rem;
