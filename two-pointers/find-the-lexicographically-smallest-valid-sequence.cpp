310c354d762424f8ed66bc9872c78e152361ceb5
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-09 00:15:04 +0530

    LC-find-the-lexicographically-smallest-valid-sequence [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<int> occur(n, 0);
+        int i = n - 1;
+        int j = m - 1;
+        int isequal = 0;
+        while (i >= 0) {
+            if (word1[i] == word2[j]) {
+                isequal++;
+                occur[i] = isequal;
+                i--;
+                j--;
+            } else {
+                occur[i] = isequal;
+                i--;
+            }
+        }
+        i = 0;
+        j = 0;
+        vector<int> seq;
+        bool canChange = true;
+        while (i < n && j < m) {
+            if (word1[i] == word2[j]) {
+                seq.push_back(i);
+                j++;
+            } else if (canChange && i + 1 < n && occur[i + 1] >= (m - j - 1)) {
+                seq.push_back(i);
+                j++;
+                canChange = false;
+            }
+            i++;
+        }
+        if (j >= m)
+            return seq;
+        return {};
+    }
+};
