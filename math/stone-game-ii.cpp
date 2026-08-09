966f501140c78bb73002c054c36f0f9745c46e17
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-09 22:50:57 +0530

    LC-stone-game-ii [Runtime: 13 ms; Memory: 10.9 MB]

--- math/stone-game-ii.cpp
+++ math/stone-game-ii.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+    int dp[101][101][2];
     int AliceScore(vector<int>& piles, int idx, int M, int turn) {
         int n = piles.size();
         if (idx >= n)
             return 0;
+        if (dp[idx][M][turn] != -1)
+            return dp[idx][M][turn];
         int stones = 0;
         int result = (turn == 1) ? INT_MIN : INT_MAX;
         for (int x = 1; x <= min(2 * M, n - idx); x++) {
@@ -14,9 +17,10 @@ public:
                 result = min(result, AliceScore(piles, idx + x, max(M, x), 1));
             }
         }
-        return result;
+        return dp[idx][M][turn] = result;
     }
     int stoneGameII(vector<int>& piles) {
+        memset(dp, -1, sizeof(dp));
         return AliceScore(piles, 0, 1, 1);
     }
 };
