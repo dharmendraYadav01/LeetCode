111e1d8a1758fe05a3b8073a74d9b38399530aa3
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-12 22:59:29 +0530

    LC-length-of-longest-subarray-with-at-most-k-frequency [Runtime: N/A; Memory: N/A]

--- sliding-window/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ sliding-window/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -7,17 +7,14 @@ public:
         unordered_map<int, int> freq;
         int result = INT_MIN;
         while (i < n && j < n) {
-            // freq[nums[j]]<=k || !freq.count(nums[j])){
             freq[nums[j]]++;
-            j++;
-
-            while (freq[nums[i]] > k) {
+            while (i < j && freq[nums[j]] > k) {
                 freq[nums[i]]--;
                 i++;
-                result = max(result, j - i + 1);
             }
             result = max(result, j - i + 1);
+            j++;
         }
-        return result - 1;
+        return result;
     }
 };
