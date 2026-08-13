ecf52ea8c5116e31e12fbf61430aa03781a53228
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-13 21:49:42 +0530

    LC-minimum-moves-to-equal-array-elements [Runtime: N/A; Memory: N/A]

--- math/minimum-moves-to-equal-array-elements.cpp
+++ math/minimum-moves-to-equal-array-elements.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+    // time complexity-> O(nlogn)
+        // sort(nums.begin(), nums.end());
+        // int n = nums.size();
+        // int gap = 0;
+        // for (int i = n - 1; i >= 0; i--) {
+        //     gap += nums[i] - nums[0];
+        // }
+        // return gap;
+    // time complexity-> O(n)
         int n = nums.size();
-        int gap = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            gap += nums[i] - nums[0];
-        }
-        return gap;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int mini = *min_element(nums.begin(), nums.end());
+        return sum - (n * mini);
     }
 };
