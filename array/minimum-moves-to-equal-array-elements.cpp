30ea093dd41386ec233eee85071e70b72f1cda71
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-13 22:05:44 +0530

    LC-minimum-moves-to-equal-array-elements [Runtime: N/A; Memory: N/A]

--- array/minimum-moves-to-equal-array-elements.cpp
+++ array/minimum-moves-to-equal-array-elements.cpp
@@ -10,9 +10,16 @@ public:
         // }
         // return gap;
     // time complexity-> O(n)
-        int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
+        // int n = nums.size();
+        // int sum = accumulate(nums.begin(), nums.end(), 0);
+        // int mini = *min_element(nums.begin(), nums.end());
+        // return sum - (n * mini);
+    // also in O(n)
         int mini = *min_element(nums.begin(), nums.end());
-        return sum - (n * mini);
+        int gap = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            gap += nums[i] - mini;
+        }
+        return gap;
     }
 };
