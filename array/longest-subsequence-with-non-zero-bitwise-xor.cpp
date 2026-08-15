22b43c44726356aabbd50ab6a7147bdb478929fd
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-15 10:01:59 +0530

    LC-longest-subsequence-with-non-zero-bitwise-xor [Runtime: 0 ms; Memory: 171.1 MB]

--- array/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ array/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
-        int Xor=0;
-        for(int it:nums){
-            Xor^=it;
+        int Xor = 0;
+        for (int it : nums) {
+            Xor ^= it;
         }
-        if(Xor!=0){
+        if (Xor != 0) {
             return nums.size();
         }
-        bool flag=false;
-        for(int it:nums){
-            if((Xor^it)!=0){
-                flag=true;
+        bool flag = false;
+        for (int it : nums) {
+            if ((Xor ^ it) != 0) {
+                flag = true;
                 break;
             }
         }
-        return flag?nums.size()-1:0;
+        return flag ? nums.size() - 1 : 0;
     }
 };
