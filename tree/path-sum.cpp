8bbd8a6ee991da5437c649a043a686560577f466
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-17 23:09:42 +0530

    LC-path-sum [Runtime: N/A; Memory: N/A]

--- tree/path-sum.cpp
+++ tree/path-sum.cpp
@@ -6,17 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
- */ 
+ */
 class Solution {
 public:
-    bool sum(TreeNode* root, int targetSum){
-        if(!root) return false;
-        if(!root->left && !root->right) return root->val==targetSum;
-        return sum(root->left,targetSum-root->val) || sum(root->right,targetSum-root->val);
+    bool sum(TreeNode* root, int targetSum) {
+        if (!root)
+            return false;
+        if (!root->left && !root->right)
+            return root->val == targetSum;
+        return sum(root->left, targetSum - root->val) ||
+               sum(root->right, targetSum - root->val);
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        return sum(root,targetSum);
+        // Is it root to leaf sum?
+        return sum(root, targetSum);
     }
 };
