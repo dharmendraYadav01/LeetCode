bd3dea16239dd33ea6815cd45a8ce92be7d76f25
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-22 09:35:02 +0530

    LC-check-divisibility-by-digit-sum-and-product [Runtime: 0 ms; Memory: 7.7 MB]

--- math/check-divisibility-by-digit-sum-and-product.cpp
+++ math/check-divisibility-by-digit-sum-and-product.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int product(int n){
-        int pro=1;
-        while(n!=0){
-            pro*=n%10;
-            n/=10;
+    int product(int n) {
+        int pro = 1;
+        while (n != 0) {
+            pro *= n % 10;
+            n /= 10;
         }
         return pro;
     }
-    int sum(int n){
-        int sum=0;
-        while(n!=0){
-            sum+=n%10;
-            n/=10;
+    int sum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
         }
         return sum;
     }
     bool checkDivisibility(int n) {
-        int a=sum(n);
-        int b=product(n);
-        int sum=a+b;
-        if(n%sum==0) return true;
+        int a = sum(n);
+        int b = product(n);
+        int sum = a + b;
+        if (n % sum == 0)
+            return true;
         return false;
     }
 };
