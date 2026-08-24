199acd3a0dec662aea6a777e9de96a2894a1c384
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-25 00:14:24 +0530

    LC-combine-two-tables [Runtime: N/A; Memory: N/A]

--- database/combine-two-tables.sql
+++ database/combine-two-tables.sql
@@ -1,5 +1,2 @@
 # Write your MySQL query statement below
-select firstName,lastName,city,state
-from Person p
-left JOIN Address a
-on p.personId=a.personId;
+select p.firstName , p.lastName , a.city , a.state from Person p left join address a on p.personId=a.personId;
