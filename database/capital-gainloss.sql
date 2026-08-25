e901c763da5f04ff3614b85bc4d9a0dee521bbf7
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-26 04:01:20 +0530

    LC-capital-gainloss [Runtime: N/A; Memory: N/A]

---
+++ database/capital-gainloss.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+Select stock_name , sum(case when operation='Sell' then price else 0 end) - sum(case when operation='Buy' then price else 0 end) as capital_gain_loss from Stocks group by stock_name;
