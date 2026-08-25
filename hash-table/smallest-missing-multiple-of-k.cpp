64cecb73dd37579df1d62b6a30ed24d3aa06915a
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-25 11:46:23 +0530

    LC-smallest-missing-multiple-of-k [Runtime: N/A; Memory: N/A]

--- hash-table/smallest-missing-multiple-of-k.cpp
+++ hash-table/smallest-missing-multiple-of-k.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
         unordered_set<int> st(nums.begin(), nums.end());
-        int i = 1;
-        int n = nums.size();
-        while (true) {
-            if (i % k == 0 && !st.count(i)) {
+        for (int i = k;; i += k) {
+            if (!st.count(i))
                 return i;
-            }
-            i++;
         }
+        // int i = 1;
+        // int n = nums.size();
+        // while (true) {
+        //     if (i % k == 0 && !st.count(i)) {
+        //         return i;
+        //     }
+        //     i++;
+        // }
         return -1;
     }
 };
