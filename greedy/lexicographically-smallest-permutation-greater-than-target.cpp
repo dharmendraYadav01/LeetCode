5ddad1d7472f2773d71c698a5ee8f94ab3c3babc
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-27 12:45:07 +0530

    LC-lexicographically-smallest-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

--- greedy/lexicographically-smallest-permutation-greater-than-target.cpp
+++ greedy/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-    string lexGreaterPermutation(string s, string target) {
-
-        string ans = s;
-        string res = "";
-        sort(ans.begin(), ans.end());
-        if (ans > target)
-            return ans;
-        while (next_permutation(ans.begin(), ans.end())) {
-            if (ans > target) {
-                return ans;
+    string ans = "";
+    bool Solve(vector<int>& freq, string curr, string& target, int idx, bool greater) {
+        if (idx == target.length()) {
+            if (greater) {
+                ans = curr;
+                return true;
             }
+            return false;
         }
-        return "";
+        for (char c = 'a'; c <= 'z'; c++) {
+            // base case
+            if (freq[c - 'a'] == 0)
+                continue;
+            if (!greater && c < target[idx])
+                continue;
+            // do
+            curr.push_back(c);
+            freq[c - 'a']--;
+            bool isvalid = greater || c > target[idx];
+            if (Solve(freq, curr, target, idx + 1, isvalid))
+                return true;
+            //backtracking
+            curr.pop_back();
+            freq[c - 'a']++;
+        }
+        return false;
+    }
+    string lexGreaterPermutation(string s, string target) {
+        vector<int> freq(26, 0);
+        for (char& c : s) {
+            freq[c - 'a']++;
+        }
+        Solve(freq, "", target, 0, false);
+        return ans;
     }
 };
