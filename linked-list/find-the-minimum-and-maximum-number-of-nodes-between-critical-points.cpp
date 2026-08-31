ab0ddce4db1f0d9e796d86807d35b8e65cb14e04
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-31 23:36:49 +0530

    LC-find-the-minimum-and-maximum-number-of-nodes-between-critical-points [Runtime: N/A; Memory: N/A]

--- linked-list/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ linked-list/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,9 +11,39 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+    // first Approach :
+        // vector<int> temp;
+        // ListNode* ptr = head;
+        // while (ptr) {
+        //     temp.push_back(ptr->val);
+        //     ptr = ptr->next;
+        // }
+        // vector<int> res;
+        // for (int i = 1; i < temp.size() - 1; i++) {
+        //     if (temp[i] > temp[i - 1] && temp[i] > temp[i + 1]) {
+        //         res.push_back(i + 1);
+        //     }
+        //     if (temp[i] < temp[i - 1] && temp[i] < temp[i + 1]) {
+        //         res.push_back(i + 1);
+        //     }
+        // }
+        // int mini = INT_MAX;
+        // int maxi = -1;
+        // for (int i = 1; i < res.size(); i++) {
+        //     mini = min(mini, res[i] - res[i - 1]);
+        // }
+        // if (res.size() > 1) {
+        //     maxi = res[res.size() - 1] - res[0];
+        // }
+        // if (maxi == -1 || mini == INT_MAX) {
+        //     return {-1, -1};
+        // }
+        // return {mini, maxi};
+    
+    // Second Approach : 
         ListNode* prev = head;
         ListNode* curr = prev->next;
-        int pos = 2;
+        int pos = 2; // start with idx 1 & first node is not local maxi or mini;
         int first_c = 0;
         int prev_c = 0;
         int mini = INT_MAX;
