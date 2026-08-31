51750870fd751682e96218e750f5f9f5edf448d6
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-08-31 23:33:04 +0530

    LC-find-the-minimum-and-maximum-number-of-nodes-between-critical-points [Runtime: N/A; Memory: N/A]

--- linked-list/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ linked-list/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,32 +11,37 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> temp;
-        ListNode* ptr = head;
-        while (ptr) {
-            temp.push_back(ptr->val);
-            ptr = ptr->next;
-        }
-        vector<int> res;
-        for (int i = 1; i < temp.size() - 1; i++) {
-            if (temp[i] > temp[i - 1] && temp[i] > temp[i + 1]) {
-                res.push_back(i + 1);
-            }
-            if (temp[i] < temp[i - 1] && temp[i] < temp[i + 1]) {
-                res.push_back(i + 1);
-            }
-        }
+        ListNode* prev = head;
+        ListNode* curr = prev->next;
+        int pos = 2;
+        int first_c = 0;
+        int prev_c = 0;
         int mini = INT_MAX;
         int maxi = -1;
-        for (int i = 1; i < res.size(); i++) {
-            mini = min(mini, res[i] - res[i - 1]);
+        while (curr->next) {
+            bool critical = false;
+            if (curr->val > prev->val && curr->val > curr->next->val) {
+                critical = true;
+            }
+            if (curr->val < prev->val && curr->val < curr->next->val) {
+                critical = true;
+            }
+            if (critical) {
+                if (first_c == 0) {
+                    first_c = pos;
+                }
+                if (prev_c) {
+                    mini = min(mini, pos - prev_c);
+                }
+                prev_c = pos;
+            }
+            prev = curr;
+            curr = curr->next;
+            pos++;
         }
-        if (res.size() > 1) {
-            maxi = res[res.size() - 1] - res[0];
-        }
-        if (maxi == -1 || mini == INT_MAX) {
+        if (mini == INT_MAX)
             return {-1, -1};
-        }
+        maxi = prev_c - first_c;
         return {mini, maxi};
     }
 };
