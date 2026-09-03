5813f891020e852ac0d0492dcad67ecaf38199a8
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-09-03 23:47:51 +0530

    LC-construct-uniform-parity-array-ii [Runtime: N/A; Memory: N/A]

--- math/construct-uniform-parity-array-ii.cpp
+++ math/construct-uniform-parity-array-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
+        // if smallest element is odd then always return true;
         int mini = nums1[0];
         for (int i = 1; i < nums1.size(); i++) {
             if (mini > nums1[i])
@@ -9,10 +10,11 @@ public:
         if (mini % 2 == 1)
             return true;
 
+        // then smallest element is even then check their exist any odd number
         bool anyOdd = false;
         for (int i = 0; i < nums1.size(); i++) {
             if (nums1[i] % 2 == 1)
-                anyOdd = true;
+                anyOdd = true; // if odd element present then we never form all even or all odd parity in nums2 it always gives false answer.
         }
         return anyOdd == true ? false : true;
     }
