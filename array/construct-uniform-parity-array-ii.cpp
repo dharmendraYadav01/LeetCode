75346cabdd8e1630c946a4a8ea99ef3222b3c03a
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-09-03 12:44:54 +0530

    LC-construct-uniform-parity-array-ii [Runtime: N/A; Memory: N/A]

---
+++ array/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        sort(nums1.begin(), nums1.end());
+        for (int i = 1; i < nums1.size(); i++) {
+            if (nums1[i] - nums1[i - 1] <= 1)
+                return false;
+        }
+        return true;
+    }
+};
