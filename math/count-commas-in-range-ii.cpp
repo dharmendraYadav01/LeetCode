0546b3676576618a2d9d399f859f76095c0e2bfe
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-09-09 15:10:11 +0530

    LC-count-commas-in-range-ii [Runtime: N/A; Memory: N/A]

--- math/count-commas-in-range-ii.cpp
+++ math/count-commas-in-range-ii.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    long long ll;
+
     long long countCommas(long long n) {
-        if (n < 1000)
-            return 0;
-        long long res = 0;
-        if (n >= 1000) {
-            res += n - 1000 + 1;
+        long long curr = 1000;
+        long long ans = 0;
+        while (curr <= n) {
+            ans += n - curr + 1;
+            curr *= 1000;
         }
-        if (n >= 1000000) {
-            res += min(n,999999999ll) - 1000000 + 1;
-        }
-        if (n >= 1000000000) {
-            res += min(n,999999999999ll) - 1000000000 + 1;
-        }
-        if (n >= 1000000000000) {
-            res += min(n,999999999999999ll) - 1000000000000 + 1;
-        }
-        if (n >= 1000000000000000 ) {
-            res += min(n,999999999999999999ll) - 1000000000000000 + 1;
-        }
-        return res;
+        return ans;
     }
 };
