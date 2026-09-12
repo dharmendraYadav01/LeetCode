cee55d77b4796a4dd28c6480d40a9be10eb0cc74
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-09-13 00:13:55 +0530

    LC-tree-node [Runtime: 473 ms; Memory: 0B]

--- database/tree-node.sql
+++ database/tree-node.sql
@@ -3,6 +3,6 @@ Select id ,
     Case when p_id is NULL then 'Root'
     when id in(
         select p_id from tree
-    )then 'Inner'
+    )then 'Inner' -- if it is inner
     else 'Leaf'
 End as type from Tree;
