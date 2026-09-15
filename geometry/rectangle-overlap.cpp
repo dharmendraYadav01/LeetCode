ecb771e5b2a13de83b7c40445db7b29ed3f93c88
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-09-15 23:40:01 +0530

    LC-rectangle-overlap [Runtime: N/A; Memory: N/A]

--- geometry/rectangle-overlap.cpp
+++ geometry/rectangle-overlap.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        int r1_x1= rec1[0];
-        int r1_y1= rec1[1];
-        int r1_x2= rec1[2];
-        int r1_y2= rec1[3];
-        int r2_x1= rec2[0];
-        int r2_y1= rec2[1];
-        int r2_x2= rec2[2];
-        int r2_y2= rec2[3];
-        return r1_x2> r2_x1 && r1_y2>r2_y1?true:false;
-         
+        int r1_x1 = rec1[0];
+        int r1_y1 = rec1[1];
+        int r1_x2 = rec1[2];
+        int r1_y2 = rec1[3];
+        int r2_x1 = rec2[0];
+        int r2_y1 = rec2[1];
+        int r2_x2 = rec2[2];
+        int r2_y2 = rec2[3];
+        return (r1_x2 > r2_x1 && r1_x1 < r2_x2) && (r1_y2 > r2_y1 && r1_y1 < r2_y2) ? true : false;
     }
 };
