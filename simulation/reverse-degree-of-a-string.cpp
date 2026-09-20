df54f4fb6a2df23c2876f74f2c80d844affad0bf
dharmendra_Yadav29 <160862552+dharmendraYadav01@users.noreply.github.com>
2026-09-20 11:28:02 +0530

    LC-reverse-degree-of-a-string [Runtime: N/A; Memory: N/A]

--- simulation/reverse-degree-of-a-string.cpp
+++ simulation/reverse-degree-of-a-string.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        unordered_map<char, int> mp;
-        int i = 1;
-        for (char c = 'z'; c >= 'a'; c--) {
-            mp[c] = i;
-            i++;
-        }
         int sum = 0;
         for (int i = 0; i < s.length(); i++) {
-            sum += mp[s[i]] * (i + 1);
+            sum += (i + 1) * (26 - (s[i] - 'a'));
         }
         return sum;
     }
